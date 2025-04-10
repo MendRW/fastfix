85b83e7f2e5f7a8325f3d232e24addf2c4dd035f
MendRW <rwilc13@gmail.com>
2025-04-10 22:12:09 +1000

    Update protofast.py

--- Prototype/protofast.py
+++ Prototype/protofast.py
@@ -1,9 +1,19 @@
 import pandas as pd
-colfilter = [] # put the index of the columns you want to pull
-resolvedby = ["dean.lynch"] #filter out other teams by choosing which person resolved ticket
-df = pd.read_csv() #reads the csv file
 
 def main():
-    df = (df.iloc[:, colfilter]) & (df [resolvedby])
-    df.to_csv('new_file.csv', index=False)
+    # Define filters
+    colfilter = []  # put the index of the columns you want to pull
+    resolvedby = ["dean.lynch"]  # filter out other teams by choosing which person resolved ticket
+    
 
+    df = pd.read_csv('your_file.csv')
+     
+    filtered_df = df.iloc[:, colfilter]
+    filtered_df = filtered_df[filtered_df['ResolvedBy'].isin(resolvedby)]
+    
+    # Save to new CSV
+    filtered_df.to_csv('new_file.csv', index=False)
+
+
+if __name__ == "__main__":
+    main()
