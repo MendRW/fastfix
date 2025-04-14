2dc1c552b710598e13a399cf3fffdcef97d0e269
MendRW <rwilc13@gmail.com>
2025-04-14 22:24:57 +1000

    Update fastscript.py

--- Prototype/fastscript.py
+++ Prototype/fastscript.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from openpyxl import load_workbook
 
-file_name = "" #file name, place the file in the same folder as script
+file_name = "" #file name, place the file in the same folder as script, i'll probably make this an input or even read from clipboard
 cols = []   #which cols you want to use 
 
 df = pandas.read_excel(file_name, header=0, usecols=cols )
@@ -12,6 +12,8 @@ with pd.ExcelWriter(file_name, engine='openpyxl', mode='a') as writer:
     writer.book = book
     writer.sheets = {ws.title: ws for ws in book.worksheets}
 
+    df.to_excel(writer, sheet_name='CleanData', index=False)
 
+    writer.save()
 
 # future note to use lambda functions for pattern matching in the RFA message text
