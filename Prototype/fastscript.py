a41e628eb0565f457cd4cd4420a16e85896aa467
MendRW <rwilc13@gmail.com>
2025-04-14 22:50:13 +1000

    Update fastscript.py

--- Prototype/fastscript.py
+++ Prototype/fastscript.py
@@ -1,19 +1,55 @@
 import pandas as pd
 from openpyxl import load_workbook
 
-file_name = "" #file name, place the file in the same folder as script, i'll probably make this an input or even read from clipboard
-cols = []   #which cols you want to use 
+# Global variables
+file_name = ""  # Will be set in setup()
+cols = []       # Which cols you want to use
+sheet_name = '' # Will be set in setup()
+df = None       # Will hold the DataFrame
 
-df = pandas.read_excel(file_name, header=0, usecols=cols )
+def setup():
+    global file_name, sheet_name, cols
+    
+    file_name = input("Enter File Name: ")
+    if not file_name.lower().endswith(('.xlsx', '.xls')):
+        file_name = file_name + '.xlsx'
+        print(f"Using file name: {file_name}")
+    
+    # Get columns to use
+    cols_input = input("Enter column names or indices separated by commas: ")
+    cols = [col.strip() for col in cols_input.split(',')]
+    
+    # Get sheet name
+    sheet_name = input("Enter name for the new sheet: ")
 
-book = load_workbook(file_name)
+def load_data():
+    global df
+    df = pd.read_excel(file_name, header=0, usecols=cols)
+    print("Data loaded:")
+    print(df)
 
-with pd.ExcelWriter(file_name, engine='openpyxl', mode='a') as writer:
-    writer.book = book
-    writer.sheets = {ws.title: ws for ws in book.worksheets}
+def check_name():
+    global book
+    book = load_workbook(file_name)
+    if sheet_name in book.sheetnames:
+        response = input(f"Sheet '{sheet_name}' already exists. Replace it? (yes/no): ")
+        if response.lower() == 'yes':
+            book.remove(book[sheet_name])
+            print(f"Removed existing sheet '{sheet_name}'")
+        else:
+            print("Operation cancelled.")
+            exit()
 
-    df.to_excel(writer, sheet_name='CleanData', index=False)
+def write_sheet():
+    with pd.ExcelWriter(file_name, engine='openpyxl', mode='a') as writer:
+        writer.book = book
+        writer.sheets = {ws.title: ws for ws in book.worksheets}
+        df.to_excel(writer, sheet_name=sheet_name, index=False)
+        print(f"Data successfully written to sheet '{sheet_name}'")
 
-    writer.save()
-
-# future note to use lambda functions for pattern matching in the RFA message text
+# Execute the functions
+if __name__ == "__main__":
+    setup()
+    load_data()
+    check_name()
+    write_sheet()
