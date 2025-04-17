27617399c9b7ee74f3aaa71652b41435ee17b78c
MendRW <145011722+MendRW@users.noreply.github.com>
2025-04-17 15:06:01 +1000

    1.0

--- Prototype/fastscript.py
+++ Prototype/fastscript.py
@@ -1,57 +1,55 @@
 import pandas as pd
+import openpyxl
 from openpyxl import load_workbook
-# it probably makes sense to remove most of the inputs here and just make people edit the script. especially the columns 
-# Global variables
-file_name = ""  # you should need the file to be in the same folder as the script, you don't need the file extension if you dont want it. the script should handle it
-cols = []       # Which cols you want to use, You can use names or index, I could also allow you to use lettered cols if you really wanted
-sheet_name = '' # This likely could just be "Clean_data" or something if the input is annoying
-df = None       # Will hold the DataFrame
+from openpyxl.styles import Alignment
 
+#You need to install both pandas and openpyxl > type into your cmd: pip install pandas openpyxl
+
+# Global variables
+file_name = ""  
+cols = ["ID","TeamManager","Message","Complexity","SkillSet","RequestTopic","ResolutionDetails","RequestedBy","RequestResolvedBy","Resolution","Rating","RequestorKnowledgeBase","RequestorTicketNumber"]  # Columns to use
+sheet_name = "Data"  # Worksheet name
+output = r"C:\Users\rory.wilcox\Desktop\Fast\output.xlsx"     #You need to input your desired destination where the file will be saved, it may need to be local
+client_list = ["OIR","DPC","PIM","QLDRA","OIC"]
 def setup():
-    global file_name, sheet_name, cols
-    
-    file_name = input("Enter File Name: ")
-    if not file_name.lower().endswith(('.xlsx', '.xls')):
+    global file_name
+    file_name = input("Enter File Name: ")#asks user for the target file
+    if not file_name.lower().endswith(('.xlsx', '.xls')):#if the user forgot to add the file extension this should handle it
         file_name = file_name + '.xlsx'
-        print(f"Using file name: {file_name}")
-    
-    # Get columns to use
-    cols_input = input("Enter column names or indices separated by commas: ")
-    cols = [col.strip() for col in cols_input.split(',')]
-    
-    # Get sheet name
-    sheet_name = input("Enter name for the new sheet: ")
+    print(f"Using file name: {file_name}")
 
 def load_data():
     global df
     df = pd.read_excel(file_name, header=0, usecols=cols)
     print("Data loaded:")
-    print(df)
-
-def check_name():
-    global book
-    book = load_workbook(file_name)
-    if sheet_name in book.sheetnames:
-        response = input(f"Sheet '{sheet_name}' already exists. Replace it? (yes/no): ")
-        if response.lower() == 'yes':
-            book.remove(book[sheet_name])
-            print(f"Removed existing sheet '{sheet_name}'")
-        else:
-            print("Operation cancelled.")
-            exit()
+    print(df)#Printing the DF here so the user at a glance can see if it's run properly
 
 def write_sheet():
-    with pd.ExcelWriter(file_name, engine='openpyxl', mode='a') as writer:
-        writer.book = book
-        writer.sheets = {ws.title: ws for ws in book.worksheets}
-        df.to_excel(writer, sheet_name=sheet_name, index=False)
-        print(f"Data successfully written to sheet '{sheet_name}'")
+    global writer
+    writer = pd.ExcelWriter(output, engine="openpyxl") #Initialising the writer object which will turn our DF into an excel doc
+    df.to_excel(writer, index=False, sheet_name=sheet_name)
+    writer.close()
 
+def format_sheet():
+    # Load the saved workbook
+    workbook = load_workbook(output)
+    worksheet = workbook[sheet_name]
+    max_length = 0
+    
+    for column_cells in worksheet.columns:
+        column_letter = column_cells[0].column_letter
+
+        for cell in column_cells:
+            cell.alignment = Alignment(wrap_text=True)
+            if cell.value:
+                max_length = max(max_length, len(str(cell.value)))
+                new_width = max_length + 1
+                worksheet.column_dimensions[column_letter].width = new_width
+    # Save the formatted workbook
+    workbook.save(output)
+   
 if __name__ == "__main__":
-    setup()
+    setup() 
     load_data()
-    check_name()
-    # should be able to slap in your own functions here if you want to do some fancy things to the sheet
     write_sheet()
-
-
+    format_sheet()
