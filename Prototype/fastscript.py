9c5afc71d0834b99f54524986e86b33904cd4969
MendRW <rwilc13@gmail.com>
2025-04-27 20:40:11 +1000

    Moved variables, changed column logic
    Variables

--- Prototype/fastscript.py
+++ Prototype/fastscript.py
@@ -3,24 +3,6 @@ from config import *
 
 #You need to install both pandas and openpyxl > type into your cmd: pip install pandas openpyxl
 
-# Global variables
-file_name = ""  
-cols = ["ID","TeamManager","Message","Complexity","SkillSet","RequestTopic","ResolutionDetails","RequestedBy","RequestResolvedBy","Resolution","Rating","RequestorKnowledgeBase","RequestorTicketNumber"]  # Columns to use
-sheet_name = "Data"  # Worksheet name
-output = r"C:\Users\rwilc\OneDrive\Documents\GitHub\Fast\fastfix\Prototype"     #You need to input your desired destination where the file will be saved, it may need to be local
-
-client_list = {
-    "oir":["oir", "office of indsutrial relations", "the office of industrial relations", "industrial relations", "wrc", "workers comp", "workers compensation","qirc"],
-    "pinnacle":["pim", "pinnacle", "palisade", "hyperion", "maplebrown", "maple-brown", "maplebrown abbott","maple-brown abott", "maple-brown abbott", "anitpodes", "pinnacle investments","palisade asset management","firetrail","hyperion","plato","spheria","two trees","solaris","res cap","rez cap","resolution capital"],
-    "oic":["oic", "office of the information commissioner", "office of information comissioner", "office information comissioner"],
-    "qldra":["qldra", "qra", "queensland reconstruction authority", "reconstruction authority", "queensland reconstruction"],
-    "nsu":["nsu", "neurosensory","neuro","neurosense"],
-    "dpc":["dpc", "department premier cabinet", "departmentofthepremiercabinet", "psc","premiers","oog","oqpc","tcis","integrity commissioner"],
-    "ozcare":["ozcare", "oz care", "oscare","ozc"],
-    "qtco":["qtco","queensland treasury corporation","qtc"],
-    "opq":["opq"],
-    "olgr":["olgr"]
-    }
 
 def setup():
     global file_name
